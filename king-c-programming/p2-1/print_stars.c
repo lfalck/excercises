716c5c3cc0557c3527cf2a830730ba1fd4d96182
Ludvig Falck <lfalck@gmail.com>
2021-03-31 22:41:45 +0200

    Added king-c-programming/p2-1

---
+++ king-c-programming/p2-1/print_stars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%8s\n", "*");
+    printf("%7s\n", "*");
+    printf("%6s\n", "*");
+    printf("%s", "*");
+    printf("%4s\n", "*");
+    printf("%2s", "*");
+    printf("%2s\n", "*");
+    printf("%3s\n", "*");
+
+    return 0;
+}
