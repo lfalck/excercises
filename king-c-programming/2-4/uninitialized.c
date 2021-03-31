5ee182445592847e11ec095a9eed3e26b85187f5
Ludvig Falck <lfalck@gmail.com>
2021-03-31 22:20:55 +0200

    Added king-c-programming/2-4/uninitialized.c

---
+++ king-c-programming/2-4/uninitialized.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i1;
+    int i2;
+    float f1;
+    float f2;
+
+    printf("%d\n", i1);
+    printf("%d\n", i2);
+    printf("%f\n", f2);
+    printf("%f\n", f1);
+
+    return 0;
+}
